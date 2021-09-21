6cac31b24753db32fa34104e934808af6d6b6603
fasthill <kangeun@naver.com>
2021-09-21 23:05:00 +0900

    collect data, preprocess data

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import pandas as pd
+import os
+from datetime import datetime
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# merging 12 months sales data into a single file
 
+all_month_data = pd.DataFrame()
+files = [pd.read_csv('./Sales_Data/'+file) for file in os.listdir('./Sales_Data')]
+for file in files:
+    all_month_data = pd.concat([all_month_data, file], ignore_index=True)
+all_month_data.to_csv('all_data.csv', index=False)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# read data
+all_data = pd.read_csv('all_data.csv')
 
+# add month columns
+all_data = all_data.dropna(how='all') # nan 제거
+all_data = all_data[all_data['Order Date'] != 'Order Date']
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+all_data['Month'] = all_data['Order Date'].str[0:2]
+all_data['Month'] = all_data['Month'].astype('int32')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Question 1 : What was the best month for sales? How much was earned that month?
+all_data['Order Date (datetime)'] = pd.to_datetime(all_data['Order Date'], format='%m/%d/%y %H:%M')
+
+print("1")
+# dd = datetime.strptime(all_month_data['Order Date'][0],'%m/%d/%y %H:%M')
+# dd1 = pd.to_datetime(all_month_data['Order Date'], format='%m/%d/%y %H:%M')
+# all_month_data['Order Date (datetime)'] = pd.to_datetime(all_month_data['Order Date'],'%m/%d/%y %H:%M')
+
